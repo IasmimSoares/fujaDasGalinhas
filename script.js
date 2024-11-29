ae549d1a358ff038667cb3320673e652616c21b4
IasmimSoares <iasmimim21@gmail.com>
2024-11-29 08:39:14 -0300

    new organization of elements
    changed html and css files

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-var galinha 
-var animacao=true;
+let controle = true;
+const element = document.getElementById("galinha");
 
-sertinterval(function(){
-    if (animacao==true){
-        galinha="galinha1.png";
-    }
-    
-
-
-
-
-},1000);
+setInterval(function () {
+	if (controle == true) {
+		element.src = './img/galinha1.png';
+		controle = false;
+	} else {
+		element.src = './img/galinha2.png';
+		controle = true;
+	}
+}, 500);
